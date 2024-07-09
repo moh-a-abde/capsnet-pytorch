3dc8f0950308a62d4ad849443b88e004abc925d8
Mohamed Abdel-Hamid <150195461+moh-a-abde@users.noreply.github.com>
2024-07-08 21:43:08 -0400

    Update net.py

--- net.py
+++ net.py
@@ -176,22 +176,28 @@ if __name__ == '__main__':
     import torch.nn.functional as F
     import gc
 
+    import os
+    from torchvision import datasets, transforms
+
+    # Set the path to the MNIST dataset
+    data_path = 'data/MNIST'
+
     # Training settings
     parser = argparse.ArgumentParser(description='CapsNet with MNIST')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
+                    help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=500, metavar='N',
-                        help='input batch size for testing (default: 1000)')
+                    help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=250, metavar='N',
-                        help='number of epochs to train (default: 10)')
+                    help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                        help='learning rate (default: 0.01)')
+                    help='learning rate (default: 0.01)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
+                    help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
+                    help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
+                    help='how many batches to wait before logging training status')
     parser.add_argument('--routing_iterations', type=int, default=3)
     parser.add_argument('--with_reconstruction', action='store_true', default=False)
     args = parser.parse_args()
@@ -201,17 +207,17 @@ if __name__ == '__main__':
     kwargs = {'num_workers': 1, 'pin_memory': True} if not args.no_cuda else {}
 
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data/MNIST', train=True, download=False,
-                       transform=transforms.Compose([
+        datasets.MNIST(data_path, train=True, download=False,
+                           transform=transforms.Compose([
                            transforms.Pad(2), transforms.RandomCrop(28),
                            transforms.ToTensor()
-                       ])),
+                   ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('data/MNIST', train=False, transform=transforms.Compose([
+        datasets.MNIST(data_path, train=False, transform=transforms.Compose([
             transforms.ToTensor()
-        ])),
+    ])),
         batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
     model = CapsNet(args.routing_iterations)
